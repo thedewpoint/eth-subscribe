7cda74fc7261aa2efb19a293bf6453c82fd6ab9e
DJ <dj.bowden7@gmail.com>
2017-05-14 23:46:19 -0400

    updated so that contract compiles

--- contracts/EthSubscribe.sol
+++ contracts/EthSubscribe.sol
@@ -17,32 +17,32 @@ contract EthSubscribe {
     function EthSubscribe() {
         mgrAddr = msg.sender;
     }
-    function stageWithdrawal interal(address _addr, uint _amt) {
+    function stageWithdrawal (address _addr, uint _amt) internal {
         pendingWithdrawals[_addr] += _amt;
     }
-    function appExists interal(string _appName) returns (bool) {
-        return apps[_appName] != "0x0";
+    function appExists (string _appName) internal returns (bool) {
+        return bytes(apps[_appName].appName).length != 0;
     }
-    function registerApp payable(string _appName, uint _subFee) returns (bool){
-        if(appExists(_appName)) { return false}
+    function registerApp (string _appName, uint _subFee) payable returns (bool){
+        if(appExists(_appName)) { return false;}
         apps[_appName] = Owner(msg.sender, _appName, _subFee);
         stageWithdrawal(mgrAddr,msg.value);
         return true;
     }
-    function subscribe payable(string _appName) returns (bool){
-        if(!appExists(_appName)) {return false};
+    function subscribe (string _appName) payable returns (bool){
+        if(!appExists(_appName)) {return false;}
         Owner owner = apps[_appName];
-        owner.customers[msg.sender] = Customer(msg.sender,msg.now,msg.value - owner.subFee);
+        owner.customers[msg.sender] = Customer(msg.sender,now,msg.value - owner.subFee);
         stageWithdrawal(owner.ownerAddr,msg.value - owner.subFee);
         return true;
     }
-    function unsubscribe external(string _appName) returns (bool){
+    function unsubscribe(string _appName) external returns (bool){
         if(!appExists(_appName)){return false;}
         stageWithdrawal(msg.sender,apps[_appName].customers[msg.sender].subPaid);
-        apps[_appName].customers[msg.sender] = "0x0";
+        delete apps[_appName].customers[msg.sender];
         return true;
     }
-    function withdrawal external () returns (bool){
+    function withdrawal() external returns (bool){
         uint amount = pendingWithdrawals[msg.sender];
         pendingWithdrawals[msg.sender] = 0;
         msg.sender.transfer(amount);
